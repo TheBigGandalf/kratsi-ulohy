2ab37c8b9cd71a9af8270a46d34c4783ea961939
Patrik <patrik.king17@gmail.com>
2020-11-16 23:59:03 +0100

    předělání závorek
    celkové přepracování kódu

--- zavorky.py
+++ zavorky.py
@@ -6,87 +6,44 @@ else:
     print("vlož pouze název souboru který chceš zkontrolovat")
     exit()
 
-spravnost_zavorek = True
+
 poloha = 0
-typ_zavorky = ""
-chyba_zavorky = 0
 
-pocet_jednoduchych = 0 # ()
-pocet_lomenych = 0 # <>
-pocet_ctvercovych = 0 # []
-pocet_slozenych = 0 # {}
-
-zavorky = [pocet_jednoduchych, pocet_lomenych, pocet_ctvercovych, pocet_slozenych]
-nazvy_zavorek = [")",">","]","}"]
-zavorecky = "()<>[]{}"
-print(zavorky)
-
-def aktualizuj():
-    global zavorky
-    zavorky = [pocet_jednoduchych, pocet_lomenych, pocet_ctvercovych, pocet_slozenych]
+zavory = "(<[{)>]}"
+posledni_zavorka = ""
+tz= []
+otevrena= []
 
 with open(input_file) as file:
     f = file.read()
     print(type(f))
 
+poloha = -1
+for znak in f:
+    poloha += 1
+    if znak in zavory:
+        print("A")
 
-for pismeno in f: # šlo by to zkrátit listem v t
+        if zavory.find(znak) < 4:
+            otevrena.append(znak)
+            print(otevrena)
 
-    if pismeno == "(":
-        pocet_jednoduchych += 1
-    elif pismeno == ")":
-        pocet_jednoduchych -= 1
+        else:
+            if znak == zavory[zavory.find(otevrena[-1]) + 4]:
+                otevrena.pop()
+            else:
+                print(f"chybí {zavory[zavory.find(otevrena[-1]) + 4]} závorka, nebo přebývá zavorka {znak} na místě {poloha} ")
+                exit()
 
-    elif pismeno == "<":
-        pocet_lomenych += 1
-    elif pismeno == ">":
-        pocet_lomenych -= 1
-
-    elif pismeno == "[":
-        pocet_ctvercovych += 1
-    elif pismeno == "]":
-        pocet_ctvercovych -= 1
-
-    elif pismeno == "{":
-        pocet_slozenych += 1
-    elif pismeno == "}":
-        pocet_slozenych -= 1
-
-
-    p=0
-    aktualizuj() #šlo by to taky udělat prostym výčtem tohle mi ale přijde takový elegantnější
-    for zavorka in zavorky:
-        if zavorka < 0 :
-
-            typ_zavorky = nazvy_zavorek[p]
-            chyba_zavorky = 1
-            break  #obě loops by šlo taky přerušit za pomocí else:  (for in:, for in: break, else: continut, break)
-        p += 1
-    else:
-        poloha += 1
-        continue
-
-    break
-
-
-
-
-
-if chyba_zavorky != 1:
-    p = 0
-
-    for zavorka in zavorky:
-
-        if zavorka > 0 :
-
-            typ_zavorky = nazvy_zavorek[p]
-            chyba_zavorky = 2
-        p += 1
-
-
-if chyba_zavorky == 0:
-    print("zavorky jsou v textu správně")
-elif chyba_zavorky == 1:
-    print(f"přebývající závorka {typ_zavorky} na místě {poloha + 1}. znaku")
+print(otevrena)
+prebyvajici = ""
+if len(otevrena) == 0:
+    print("závorky jsou správně")
+elif len(otevrena) == 1:
+    print(f"přebývá {otevrena[0]} závorka")
 else:
-    print(f"chybí {typ_zavorky}")
+    for zavorka in otevrena:
+        prebyvajici += " " + zavorka
+    print(f"přebývají {prebyvajici} závorky")
+
+
