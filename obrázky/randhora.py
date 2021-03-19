010506027bf1c56fcd7eb7ea144068a755050ce5
Patrik <patrik.king17@gmail.com>
2021-03-19 16:49:36 +0100

    klasický změny

--- obrázky/randhora.py
+++ obrázky/randhora.py
@@ -1,3 +1,6 @@
+#zkusit
+
+
 from tkinter import *
 import time
 from tkinter import messagebox
@@ -10,7 +13,10 @@ top = Tk()
 size = 500
 C = Canvas(top, bg = "#000000", height = size, width = size)
 SIZE = size
+sirska = 8
+PORADI = 0
 
+pohyby = []
 Hx = 0
 Hy = 0
 
@@ -28,31 +34,61 @@ def hexCodeFromRgb(red, green, blue):
     hexGreen = hexovac(green)
     hexBlue = hexovac(blue)
 
+
     return f'#{hexRed}{hexGreen}{hexBlue}'
 
 #opakovani, smer
 chodba = random.choice([True, False])
+class ctverec :
+    """vytvorim ctverec, zapamatuju si relativni postaveni vůči předcházejícímu čtverci, umistím ho do společného listu,
+    funkci na posouvání ctverce o pocet z listu podle poradi
+    """
+    def __init__(self,o, p):
+        
+        
+        self.pohx = 0
+        self.pohy = 0
+        self.poradi = por
+        self.sire = 0
 
-while SIZE > 10:
-    opakovani = 1 #random.randint(1, 5)
+    def vytvor(self):
+        global Hx, Hy, SIZE, sirska, pohyby
+        for vel in range(0, opakovani):
+            self.pohx = random.randint(2,5)
+            self.pohy = random.randint(2,5)
 
-    smer = random.choice([(1, 1), (1, 0), (0, 0), (0, 1)])
+            Hx += self.pohx # trochu zajímavější je možná 5*smer[0]
+            Hy += self.pohy # a 5*smer[1]
+            pohyby.append((self.pohx,self.pohy))
+            SIZE -= sirska
+            self.sire = SIZE
 
-    print(chodba)
-    for vel in range(0, opakovani):
+            if SIZE > 2 and chodba == True:
+                barva = hexCodeFromRgb(random.randint(0, int(SIZE/3)), random.randint(0, int(SIZE/3)), random.randint(0, int(SIZE/3)))
+                self.ctvr = C.create_rectangle(Hx, Hy, Hx + SIZE, Hy + SIZE, fill=barva)
+            elif SIZE > 2 :
+                barva = hexCodeFromRgb(random.randint(100 - int(SIZE/5), 200), random.randint(100 - int(SIZE/5), 200), random.randint(100 - int(SIZE/5), 200))
+                self.ctvr = C.create_rectangle(Hx, Hy, Hx + SIZE, Hy + SIZE, fill=barva)
+            else:
+                break
 
-        Hx += random.randint(2,5)  # trochu zajímavější je možná 5*smer[0]
-        Hy += random.randint(2,5)# a 5*smer[1]
-        SIZE -= 7
+    def jdi (self):
+        global pohyby,PORADI, top
+        top.move(self.ctvr,pohyby[PORADI][0],pohyby[PORADI][1])
+        self.ctvr
 
-        if SIZE > 2 and chodba == True:
-            barva = hexCodeFromRgb(random.randint(0, int(SIZE/3)), random.randint(0, int(SIZE/3)), random.randint(0, int(SIZE/3)))
-            C.create_rectangle(Hx, Hy, Hx + SIZE, Hy + SIZE, fill=barva)
-        elif SIZE > 2 :
-            barva = hexCodeFromRgb(random.randint(100 - int(SIZE/5), 200), random.randint(100 - int(SIZE/5), 200), random.randint(100 - int(SIZE/5), 200))
-            C.create_rectangle(Hx, Hy, Hx + SIZE, Hy + SIZE, fill=barva)
-        else:
-            break
+
+
+
+for p in range(1,SIZE/ sirska + 1):
+    opakov = 1 #random.randint(1, 5)
+
+
+    #smer = random.choice([(1, 1), (1, 0), (0, 0), (0, 1)])
+
+    ctverec(opakov, p)
+
+#for x in range(0,10):
 
 
 C.pack()
--- obrázky/vzor.py
+++ obrázky/vzor.py
@@ -1,43 +1,66 @@
-# !/usr/bin/python3
+
 from tkinter import *
-
+import time
 from tkinter import messagebox
-
-top = Tk()
 import random
-seed = random.randint(0,10000000)
+
+seed = random.randint(0, 10000000)
 print(seed)
 random.seed(seed)
-SIZE = 500
-C = Canvas(top, bg = "#000000", height = SIZE, width = SIZE)
 
-def intToHex(number):
-    hexadecimal = hex(number)[2:]
-    if len(hexadecimal) == 1:
-        return '0' + hexadecimal
+top = Tk()
+size = 500
+C = Canvas(top, bg="#000000", height=size, width=size)
+SIZE = size
+
+Hx = 0
+Hy = 0
+
+
+def hexovac(cislo):
+    hexcislo = hex(cislo)[2:]
+
+    if len(hexcislo) == 1:
+        return "0" + hexcislo
     else:
-        return hexadecimal
+        return hexcislo
+
 
 def hexCodeFromRgb(red, green, blue):
-    hexRed = intToHex(red)
-    hexGreen = intToHex(green)
-    hexBlue = intToHex(blue)
+    hexRed = hexovac(red)
+    hexGreen = hexovac(green)
+    hexBlue = hexovac(blue)
+
     return f'#{hexRed}{hexGreen}{hexBlue}'
 
-def draw_symmetricLines(start_x, start_y, end_x, end_y, color):
-    C.create_line(start_x, start_y, end_x, end_y, fill=color)
-    C.create_line(SIZE - start_x, SIZE - start_y, SIZE - end_x, SIZE - end_y, fill=color)
-    C.create_line(SIZE - start_x, start_y, SIZE - end_x, end_y, fill=color)
-    C.create_line(start_x, SIZE - start_y, end_x, SIZE - end_y, fill=color)
 
+# opakovani, smer
+chodba = random.choice([True, False])
 
-for i in range(10):
-    draw_symmetricLines(
-        random.randint(0,500),
-        random.randint(0,500),
-        random.randint(0,500),
-        random.randint(0,500),
-        hexCodeFromRgb(random.randint(0, 255), 0, 0),
-    )
+while SIZE > 10:
+    opakovani = 1  # random.randint(1, 5)
+
+    smer = random.choice([(1, 1), (1, 0), (0, 0), (0, 1)])
+
+    print(chodba)
+    for vel in range(0, opakovani):
+
+        Hx += random.randint(2, 5)  # trochu zajímavější je možná 5*smer[0]
+        Hy += random.randint(2, 5)  # a 5*smer[1]
+        SIZE -= 7
+
+        if SIZE > 2 and chodba == True:
+            barva = hexCodeFromRgb(random.randint(0, int(SIZE / 3)), random.randint(0, int(SIZE / 3)),
+                                   random.randint(0, int(SIZE / 3)))
+            C.create_rectangle(Hx, Hy, Hx + SIZE, Hy + SIZE, fill=barva)
+        elif SIZE > 2:
+            barva = hexCodeFromRgb(random.randint(100 - int(SIZE / 5), 200), random.randint(100 - int(SIZE / 5), 200),
+                                   random.randint(100 - int(SIZE / 5), 200))
+            C.create_rectangle(Hx, Hy, Hx + SIZE, Hy + SIZE, fill=barva)
+        else:
+            break
+
 C.pack()
+
 mainloop()
+
